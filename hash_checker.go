d9319ee619b8749a5afa954ef5c4f9da0cd9c2eb
Vladimir Edheldor Leksin <edheldor@gmail.com>
2019-04-04 01:35:53 +0300

    Разобрался как считать хеши строки

--- hash_checker.go
+++ hash_checker.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
-	"os"
+	//"os"
+	//"path/filepath"
 )
 
 func main() {
+    smaple := file{name: "dsd", path: "dsfdsf"}
+    smaple.calclHash()
+    fmt.Println(smaple.hash)
+
+}
+
+
+type file struct {
+	name string
+	path string
+	hash string
+}
+
+ func (item *file)  calclHash() {
+	hash := sha256.New()
+	hash.Write([]byte("HELLO"))
+	item.hash = fmt.Sprintf("%x", hash.Sum(nil))
+
 
 }

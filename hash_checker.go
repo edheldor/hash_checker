0300ae58da36c9d94da62ff2c12c4ace1237edf2
Vladimir Edheldor Leksin <edheldor@gmail.com>
2019-04-04 19:00:23 +0300

    Добавил функцию для вывода путей всех файлов в директории запуска и поддиректориях

--- hash_checker.go
+++ hash_checker.go
@@ -3,17 +3,62 @@ package main
 import (
 	"crypto/sha256"
 	"fmt"
-	//"os"
+	"io"
+	"os"
+	"path/filepath"
 	//"path/filepath"
 )
 
 func main() {
-    smaple := file{name: "dsd", path: "dsfdsf"}
-    smaple.calclHash()
-    fmt.Println(smaple.hash)
+	params := os.Args
+	params_len := len(params)
+
+	fmt.Println(params_len)
+
+	if params_len == 1 {
+		fmt.Println("Поддерживаются команды calc и check")
+	} else if params_len == 2 {
+		fmt.Println("Невозможно создать файл")
+	} else if params_len == 3 {
+		if params[1] == "calc" {
+			file, err := os.Create(params[2])
+			if err != nil {
+				fmt.Println("Невозможно создать файл")
+				os.Exit(1)
+			}
+			defer file.Close()
+			path, _ := os.Getwd()
+			filelist, err := FilePathWalkDir(path)
+			if err != nil {
+				fmt.Println("Невозможно создать список всех файлов")
+				os.Exit(1)
+			}
+			print(filelist)
+
+		} else if params[1] == "check" {
+			file, err := os.Open(params[2])
+			if err != nil {
+				fmt.Println("Невозможно открыть файл")
+			}
+			defer file.Close()
+
+			data := make([]byte, 64)
+
+			for {
+				n, err := file.Read(data)
+				if err == io.EOF {
+					break
+				}
+				fmt.Println(string(data[:n]))
+			}
+
+		} else {
+			fmt.Println("Поддерживаются команды calc и check")
+		}
+
+	}
 
 }
-
 
 type file struct {
 	name string
@@ -21,10 +66,20 @@ type file struct {
 	hash string
 }
 
- func (item *file)  calclHash() {
+func (item *file) calclHash() {
 	hash := sha256.New()
 	hash.Write([]byte("HELLO"))
 	item.hash = fmt.Sprintf("%x", hash.Sum(nil))
 
+}
 
+func FilePathWalkDir(root string) ([]string, error) {
+	var files []string
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			files = append(files, path)
+		}
+		return nil
+	})
+	return files, err
 }
